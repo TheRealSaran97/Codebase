8c94c286cec0a29dc66a0544ffb4abd58dc5b3f5
TheRealSaran <saranrahul10@gmail.com>
2025-06-13 19:06:15 +0530

    increment decrement operators

---
+++ src/ATB_EXJAVA01/operators.java
@@ -0,0 +1,53 @@
+package ATB_EXJAVA01;
+
+public class operators {
+    public static void main(String[] args){
+
+        int a = 5;
+
+        int b = a++;
+
+        System.out.println("a: " + a + ", b: " + b);//Program1 a=6 b=5//
+
+        //program2
+        int x = 1;
+
+        x = x++ + ++x;
+
+        System.out.println(x);//program2 i=1+3 I=4
+
+        //Program3
+        int y = 5;
+
+        System.out.println(y++ + ++y); //x=5+7=12
+
+        //program4
+        int z = 5;
+
+        System.out.println(++z); // Output: 6
+
+        System.out.println(z++); // Output: 6
+
+        System.out.println(z);// Output:7
+
+        //program5
+        int c = 5;
+
+        int d = c++ + ++c;
+
+        System.out.println("c: " + c); // Output: 7
+
+        System.out.println("d: " + d); // Output: 12
+
+        //program6
+        int e = 5;
+
+        int f = e++ + ++e + e++ + ++e;// Output: 5+7+7+9=28
+
+        System.out.println("e = " + e + ", f = " + f); //e=9 f=28
+
+
+
+    }
+
+}
