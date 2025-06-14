534d92200c93510b7c3d05126922994557731415
TheRealSaran <saranrahul10@gmail.com>
2025-06-14 09:33:50 +0530

    Practice program

---
+++ src/ATB_EXJAVA01/leftpattern.java
@@ -0,0 +1,13 @@
+package ATB_EXJAVA01;
+
+public class leftpattern {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
