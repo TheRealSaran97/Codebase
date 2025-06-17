6e9f03c6d9815c19148bf45d376b6530430ff0fa
TheRealSaran <saranrahul10@gmail.com>
2025-06-17 21:14:19 +0530

    june17th task

--- src/ATB_EXJAVA01/leftpattern.java
+++ src/ATB_EXJAVA01/leftpattern.java
@@ -5,7 +5,7 @@ public class leftpattern {
         int n = 5;
         for (int i = 5; i >= 1; i--) {
             for (int j = 1; j < i; j++) {
-                System.out.print("*");
+                System.out.print("&");
             }
             System.out.println(" ");
         }
