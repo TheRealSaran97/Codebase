11b03cd7d205a0a34925be81902bdaae3d64a6e4
TheRealSaran <saranrahul10@gmail.com>
2025-06-19 08:13:05 +0530

    challenge1

---
+++ src/ATB_EXJAVA01/pgtest.java
@@ -0,0 +1,16 @@
+package ATB_EXJAVA01;
+
+public class pgtest {
+    public static void main(String[] args) {
+        int a=25;
+        double b=3.14;
+        boolean test; // Declaration
+        test = true;
+        char str='A';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(test);
+        System.out.println(str);
+
+    }
+}
