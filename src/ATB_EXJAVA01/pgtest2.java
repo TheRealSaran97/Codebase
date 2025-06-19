b2eb6be3239cd1059357924f9026bb562312e916
TheRealSaran <saranrahul10@gmail.com>
2025-06-19 08:20:51 +0530

    challenge2

---
+++ src/ATB_EXJAVA01/pgtest2.java
@@ -0,0 +1,19 @@
+package ATB_EXJAVA01;
+
+public class pgtest2 {
+    public static void main(String[] args) {
+
+        int a=10;
+        int b=5;
+
+        int sum=a+b;
+        System.out.println(sum);
+        int sub=a-b;
+        System.out.println(sub);
+        int factor=a*b;
+        System.out.println(factor);
+        int mod=a%b;
+        System.out.println(mod);
+
+    }
+}
