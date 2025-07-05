711227a54ceef9250c8c9637b0409dda870c5228
TheRealSaran <saranrahul10@gmail.com>
2025-07-05 10:48:14 +0530

    inhertance

---
+++ src/ATB_EXJAVA01/childinheritance.java
@@ -0,0 +1,13 @@
+package ATB_EXJAVA01;
+
+public class childinheritance extends commantoall {
+    public static void main(String[] args) {
+            startbrowser();
+            System.out.println("started tc2 running");
+            closebrowser();
+
+
+        }
+    }
+
+
---
+++ src/ATB_EXJAVA01/commantoall.java
@@ -0,0 +1,13 @@
+package ATB_EXJAVA01;
+
+public class commantoall {
+
+        static void startbrowser(){
+            System.out.println("Browser started");
+        }
+        static void closebrowser(){
+            System.out.println("closed the browser");
+        }
+    }
+
+
